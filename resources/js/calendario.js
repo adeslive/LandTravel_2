8ca1bd606baa431eb3eec0ae16009eb20b26a02f
adeslive <adeslive@outlook.es>
2020-04-15 10:27:10 -0600

    Descargar en index

--- resources/js/calendario.js
+++ resources/js/calendario.js
@@ -52,20 +52,22 @@ $(function () {
     }, 30000);
 
     function check() {
-        let next = calendar.getEvents()[0];
-        $('#titulo').html(next.title);
+        if (calendar.getEvents().length > 0){
+            let next = calendar.getEvents()[0];
+            $('#titulo').html(next.title);
 
-        $.ajax({
-            type: "GET",
-            url: "guias/rutas/" + next.id,
-            dataType: "JSON",
-        })
-        .done(function(response){
-            $('#rutas').html('');
-            response.forEach( element => {
-                crearRuta(element.lugar, element.fecha_inicio, element.fecha_salida, element.pais, element.ciudad)
+            $.ajax({
+                type: "GET",
+                url: "guias/rutas/" + next.id,
+                dataType: "JSON",
+            })
+            .done(function(response){
+                $('#rutas').html('');
+                response.forEach( element => {
+                    crearRuta(element.lugar, element.fecha_inicio, element.fecha_salida, element.pais, element.ciudad)
+                });
             });
-        });
+        }
     }
 
     function crearRuta(inicio, hi, hs, pais, ciudad){
@@ -97,6 +99,14 @@ $(function () {
 
     calendar.render();
 
+    $('#descargar').click(function(){
+        html2canvas(document.querySelector('#region')).then(canvas => {
+            var img = canvas.toDataURL('image/jpeg');
+            var doc = new jsPDF();
+            doc.addImage(img, 'JPEG', 15, 40, 180, 120)
+            download(doc.output(), "Calendario.pdf", 'text/pdf');
+        });
+    });
 
     $('#marcar').click(function () {
         $('#modal-loading').modal('show');
@@ -130,5 +140,49 @@ $(function () {
             });
         });
     })
+
+
+
+    function download(strData, strFileName, strMimeType) {
+        var D = document,
+            A = arguments,
+            a = D.createElement("a"),
+            d = A[0],
+            n = A[1],
+            t = A[2] || "text/plain";
+    
+        //build download link:
+        a.href = "data:" + strMimeType + "," + escape(strData);
+    
+        if (window.MSBlobBuilder) {
+            var bb = new MSBlobBuilder();
+            bb.append(strData);
+            return navigator.msSaveBlob(bb, strFileName);
+        } /* end if(window.MSBlobBuilder) */
+    
+        if ('download' in a) {
+            a.setAttribute("download", n);
+            a.innerHTML = "downloading...";
+            D.body.appendChild(a);
+            setTimeout(function() {
+                var e = D.createEvent("MouseEvents");
+                e.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false,
+    false, false, 0, null);
+                a.dispatchEvent(e);
+                D.body.removeChild(a);
+            }, 66);
+            return true;
+        } /* end if('download' in a) */    
+    
+        //do iframe dataURL download:
+        var f = D.createElement("iframe");
+        D.body.appendChild(f);
+        f.src = "data:" + (A[2] ? A[2] : "application/octet-stream") + (window.btoa ? ";base64"
+    : "") + "," + (window.btoa ? window.btoa : escape)(strData);
+        setTimeout(function() {
+            D.body.removeChild(f);
+        }, 333);
+        return true;
+    }
 });
 
